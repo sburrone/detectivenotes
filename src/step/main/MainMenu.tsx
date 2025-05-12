4745d2c08d4b9f2754beb39bf79afd2b9087ee22
sburrone <nmtb4e@gmail.com>
2025-05-12 18:44:45 +0200

    color mode

--- src/step/main/MainMenu.tsx
+++ src/step/main/MainMenu.tsx
@@ -1,7 +1,7 @@
 import { FC, useState } from 'react'
 import { BoardModel } from './BoardModel.tsx'
 import MainMenuButtons from './MainMenuButtons.tsx'
-import { ColorMode, Game, Step } from '../../types.ts'
+import { Game, Step } from '../../types.ts'
 
 const MainMenu: FC<{
     setStep: (step: Step) => any
@@ -11,12 +11,7 @@ const MainMenu: FC<{
     return (
         <>
             <BoardModel hideUI={hideUI} setHideUI={setHideUI} />
-            <MainMenuButtons
-                setStep={setStep}
-                hideUI={hideUI}
-                setHideUI={setHideUI}
-                game={game}
-            />
+            <MainMenuButtons setStep={setStep} hideUI={hideUI} setHideUI={setHideUI} game={game} />
         </>
     )
 }
